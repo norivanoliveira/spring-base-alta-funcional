a00d43ebeded290e99e1661ae46a4469e40a87cf
Norivan Oliveira Silva <norivan.oliveira@gmail.com>
2022-06-25 12:54:28 -0300

    Organização

--- src/main/java/br/com/itau/cobranca/jpa/JPAConfig.java
+++ src/main/java/br/com/itau/cobranca/jpa/JPAConfig.java
@@ -1,9 +1,8 @@
-package br.com.itau.cobranca.infra;
+package br.com.itau.cobranca.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "br.com.itau.cobranca.repository")
 class JPAConfig {}
 
